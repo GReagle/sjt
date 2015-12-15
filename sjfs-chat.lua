64dff756793e1c79d5506ff50f0ed9f69c4e2d1d
Greg Reagle <greg.reagle@umbc.edu>
2015-12-15 11:23:24 -0500

    rename and trimming

--- sjfs-chat.lua
+++ sjfs-chat.lua
@@ -1,12 +1,12 @@
 #!/usr/bin/env lua
 require('strict')
-posix = require('posix')
+local P = require('posix')
 
 tail_out = assert(io.popen('tail -n +0 -f out', 'r'))
 assert(tail_out:setvbuf('no'))
-tail_out_fd = posix.fileno(tail_out)
+tail_out_fd = P.fileno(tail_out)
 
-stdin_fd = posix.fileno(io.stdin)
+stdin_fd = P.fileno(io.stdin)
 
 fds = {
 	[tail_out_fd] = { events = {IN=true} },
@@ -16,38 +16,23 @@ fds = {
 in_fifo = io.open('in', 'w')
 assert(in_fifo:setvbuf('no'))
 
---os.execute("stty cbreak")
 line = ''
 while true do
-	r = posix.poll(fds, -1)
+	r = P.poll(fds, -1)
 	if r == 0 then  -- timeout
 		break
 	elseif r == 1 then
 		if  fds[tail_out_fd].revents.IN then
-			io.write(tail_out:read('*l') .. '\n') -- '\a' for bell
+			io.write(tail_out:read('*l') .. '\a\n') -- '\a' for bell
 		end
 		if  fds[stdin_fd].revents.IN then
 			in_fifo:write(io.read('*l'))
-			--[[
-			c = io.read(1)
-			if c == '\n' then
-				in_fifo:write(line)
-				line = ''
-			elseif c == 't' then
-				io.write('\x1b[D')
-				--os.execute("tput cub1")
-				--os.execute("tput kcub1")
-			else
-				line = line .. c
-			end
-			]]
 		end
 	else
 		print "finish!"
 		break
 	end
 end
---os.execute("stty -cbreak");
 
 tail_out:flush()
 tail_out:close()
