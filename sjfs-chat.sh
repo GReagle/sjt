c1e8f933eb870bd83e878af816278671ddf7d95c
Greg Reagle <greg.reagle@umbc.edu>
2015-12-15 14:14:51 -0500

    add bell to chat

--- sjfs-chat.sh
+++ sjfs-chat.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-tail -n +0 -f out &
+tail -f -n +0 out | while IFS= read -r line
+do
+	printf '%s\a\n' "$line"
+done &
 tailpid=$!
 # Ctrl-D to end
-#cat > in  # this works fine except that it inserts extra new lines
+##cat > in  # this works except that it inserts extra new lines
 while read -r line 
 do
     printf '%s' "$line" > in
