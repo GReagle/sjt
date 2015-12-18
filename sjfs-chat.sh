f5bf7d5188c413778d1b70854d43a21bddb72fdc
Greg Reagle <greg.reagle@umbc.edu>
2015-12-18 15:11:46 -0500

    code improvements

--- sjfs-chat.sh
+++ sjfs-chat.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
-
 # Ctrl-D to end
+[ "$DEBUG" ] && set -x
 
-# style of incming message: either "no" to disable or a style for tput
-# e.g. bold, dim, blink, smul (underlined), rev (reverse), smso (standout)
-# only works if color is off
-: "${style:=no}"
-# color of incoming message: either "no" to disable or a color for tput setaf
-# e.g. 1-7
-# color output disables any style
+# For all the following configuration variables, if the variable is unset
+# or the empy string, then the feature is disabled.
+#
+# style of incming message:  a style for tput e.g. bold, dim, blink, smul
+# (underlined), rev (reverse), smso (standout) : "${style:=''}"
+#: "${style:=smul}"
+# color of incoming message:  a color for tput setaf, e.g. 1-7
 : "${color:=1}"
-# either "no" to disable or a number of milliseconds for the notify-send timeout
-: "${notify:=2000}"
+# number of milliseconds for the notify-send timeout
+: "${notify:=3000}"
 
 trap "kill 0; exit" HUP INT QUIT
 
 cat out || { echo out file required; exit 1; }
 tail -f -n0 out | while IFS= read -r line
 do
-	[ "$color" = no ] || tput setaf "$color"
-	[ "$style" = no ] || tput "$style"	
+	[ "$color" ] && tput setaf "$color"
+	[ "$style" ] && tput "$style"
 	printf '%s\n' "$line"
 	tput sgr0  # Turn off all attributes: back to normal
 	if echo "$line" | egrep -q "^[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2} [[:digit:]]{2}:[[:digit:]]{2} <${SJ_USER}@${SJ_SERVER}"; then
 		:  # this is a message that I sent myself, so don't notify
 	else
 		printf '\a'
-		[ "$notify" = no ] || notify-send -t "$notify" "$(basename $(pwd))" "$(echo "$line" | cut -d\> -f2-)"
+		[ "$notify" ] && notify-send -t "$notify" "sjfs-chat: $(basename $(pwd))" "$(echo "$line" | cut -d\> -f2-)"
 	fi
 done &
 
 ##cat > in  # this works except that it inserts extra new lines
-while read -r line 
+while read -r line
 do
     printf '%s' "$line" >> in
 done
