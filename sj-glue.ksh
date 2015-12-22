11114dc706908022ad49e54a67eaae36ad2c1996
Greg Reagle <greg.reagle@umbc.edu>
2015-12-22 12:27:04 -0500

    more improvements

--- sj-glue.ksh
+++ sj-glue.ksh
@@ -1,16 +1,37 @@
-#!/bin/ksh93
+#!/bin/sh
 # mksh does not have <() feature
 
-: "${te:=st}"  # terminal emulator e.g. xterm, st, rxvt
+: "${te:=st}"  # terminal emulator, e.g. xterm, st, rxvt
+
+state=""
+for arg; do
+	if [ "$state" ]; then
+		case "$state" in
+			-d) SJ_DIR="$arg";;
+			-u) SJ_USER="$rg";;
+			-r) SJ_RESOURCE="$rg";;
+			-s) SJ_SERVER="$arg";;
+			*)  echo invalid state;;
+		esac
+		state=""
+	else
+		case "$arg" in
+			-d | -u | -r | -s) state="$arg";;
+			-D) ;;
+			*)  printf "$(basename $0): invalid option $arg\n"; exit 1;;
+		esac
+	fi
+done
 
 [ "$SJ_DIR" = "" ] && { echo SJ_DIR is not defined; exit 1; }
 cd "$SJ_DIR" || { echo cannot cd to "$SJ_DIR"; exit 1; }
 
-$te -T sjfs-start -e sjfs-start &
+$te -T sjfs-start -e sjfs-start "$@" &
 echo After you connect, hit enter
 read line
-presence
-$te -T sjfs-online-buddies -e watch sjfs-online-buddies &
+presence -d "$SJ_DIR"
+$te -T sjfs-online-buddies -e watch sjfs-online-buddies -d "$SJ_DIR" &
+$te -T sjfs-monitor-all-chats -e sjfs-monitor-all-chats -d "$SJ_DIR" &
 
 exit 0
 
@@ -41,3 +62,14 @@ done
 #	$te -T "chat: $result" -e sjfs-chat.sh &
 #	cd "$SJ_DIR"
 #done
+
+while [ "$1" ]; do
+	case "$1" in
+		-d) SJ_DIR="$2"; shift;;
+		-u) SJ_USER="$2"; shift;;
+		-r) SJ_RESOURCE="$2"; shift;;
+		-s) SJ_SERVER="$2"; shift;;
+		*)  printf "$(basename $0): invalid option $1\n$usage\n"; exit 1;;
+	esac
+	shift
+done
--- sjfs-chat.sh
+++ sjfs-chat.sh
@@ -2,31 +2,30 @@
 # Ctrl-D to end
 [ "$DEBUG" ] && set -x
 
-# For all the following configuration variables, if the variable is unset
-# or the empy string, then the feature is disabled.
+# Set configuration variable to "no" to disable feature
 #
 # style of incming message:  a style for tput e.g. bold, dim, blink, smul
 # (underlined), rev (reverse), smso (standout)
-#: "${style:=smul}"
+: "${style:=no}"
 # color of incoming message:  a color for tput setaf, e.g. 1-7
 : "${color:=1}"
 # number of milliseconds for the notify-send timeout
-: "${notify:=3000}"
+: "${notify_duration:=2000}"
 
 trap "kill 0; exit" HUP INT QUIT EXIT
 
 cat out || { echo out file required; exit 1; }
 tail -f -n0 out | while IFS= read -r line
 do
-	[ "$color" ] && tput setaf "$color"
-	[ "$style" ] && tput "$style"
+	[ "$color" != no ] && tput setaf "$color"
+	[ "$style" != no ] && tput "$style"
 	printf '%s\n' "$line"
 	tput sgr0  # Turn off all attributes: back to normal
 	if echo "$line" | egrep -q "^[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2} [[:digit:]]{2}:[[:digit:]]{2} <${SJ_USER}@${SJ_SERVER}"; then
 		:  # this is a message that I sent myself, so don't notify
 	else
 		printf '\a'
-		[ "$notify" ] && notify-send -t "$notify" "sjfs-chat: $(basename $(pwd))" "$(echo "$line" | cut -d\> -f2-)"
+		[ "$notify_duration" != no ] && notify-send -t "$notify_duration" "sjfs-chat: $(basename $(pwd))" "$(echo "$line" | cut -d\> -f2-)"
 	fi
 done &
 
